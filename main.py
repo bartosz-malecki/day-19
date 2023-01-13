98ea203ed13fb46e0650f1589308eb64197b1808
Bartosz Malecki <bartoszmalecki90@gmail.com>
2023-01-13 14:17:14 +0100

    Turtle racing app done.

--- main.py
+++ main.py
@@ -1,33 +1,35 @@
+import random
 from turtle import Turtle, Screen
 
-tim = Turtle()
+is_race_on = False
 screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt='Which turtle will win the race?: ')
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_position = [-70, -40, -10, 20, 50, 80]
+all_turtles = []
 
+for turtle_index in range(0, 6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(colors[turtle_index])
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=y_position[turtle_index])
+    all_turtles.append(new_turtle)
 
-def move_forwards():
-    tim.forward(10)
+if user_bet:
+    is_race_on = True
 
+while is_race_on:
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_turtle = turtle.pencolor()
+            if winning_turtle == user_bet:
+                print(f"You've won! The {winning_turtle} turtle is the winner!")
+            else:
+                print(f"You've lost! The {winning_turtle} turtle is the winner!")
 
-def move_backwards():
-    tim.backward(10)
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
 
-
-def move_c_clockwise():
-    tim.left(10)
-
-
-def move_clockwise():
-    tim.right(10)
-
-
-def reset():
-    tim.reset()
-
-
-screen.listen()
-screen.onkey(key="w", fun=move_forwards)
-screen.onkey(key="s", fun=move_backwards)
-screen.onkey(key="a", fun=move_c_clockwise)
-screen.onkey(key="d", fun=move_clockwise)
-screen.onkey(key="c", fun=reset)
 screen.exitonclick()
